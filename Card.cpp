25198a0304b35671139c8e3c0f989a4db6fffec8
‪Yoad Abels‬‏ <7530950-yoadhamagniv@users.noreply.gitlab.com>
2023-05-28 18:26:40 +0300

    aaa

---
+++ Card.cpp
@@ -0,0 +1,31 @@
+//
+// Created by magshimim on 5/28/2023.
+//
+#include "Card.h"
+
+Card::Card(CardType type, const CardStats& stats) : m_effect(type), m_stats(stats)
+{
+}
+
+Card::
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
